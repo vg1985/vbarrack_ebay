e8c81dfb3b0764f6f570dc70c39d8cef7f76d0ee
Vineet <vk.agile@gmail.com>
2015-02-04 22:14:09 +0530

    fixed formula calculation issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,22 @@
 module ApplicationHelper
+  
   def new_price(formula, is_country, baseprice_details)
-    if(formula.present? && is_country.present? && baseprice_details.base_price)
-      puts "-----------#{formula}-----------"
-      formula = formula.gsub('base_price', baseprice_details.base_price.to_s)
-      begin
-        new_price = eval(formula).round(2)
-      rescue
-        new_price = '' 
-      end
-    else
-      new_price = ''
-    end 
+    new_price = ''
+    if(formula.present? && is_country.present?)
+      unless baseprice_details.present?
+        baseprice_details = Country.find_by_country(is_country)
+      end     
+      if baseprice_details.present?
+        formula = formula.gsub('base_price', baseprice_details.base_price.to_s)
+        begin
+          new_price = eval(formula).round(2)
+        rescue
+         
+        end
+      end  
+    end
+   
     new_price
   end
+
 end
